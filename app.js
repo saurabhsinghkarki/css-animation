f05ce9a2decd6420c8bfbdeba48380812b016d28
saurabhsinghkarki <104856935+saurabhsinghkarki@users.noreply.github.com>
2023-02-21 13:47:08 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const el = document.querySelector("body");
+
+el.addEventListener("mousemove", (e) => {
+  el.style.backgroundPositionX = -e.offsetX + "px";
+  el.style.backgroundPositionY = -e.offsetY + "px";
+});
+
